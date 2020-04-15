57bc99771850d31fcaab39a5d8222121925aaaf7
Hardik kumar <hardik.kumar18feb@gmail.com>
2020-04-15 19:52:37 +0530

    body

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: Colors.white,
+      ),
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
